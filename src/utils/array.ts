8b6e516c8728eb1353fb1107bc988f90216e44a9
Victor Dufayet <duftor@outlook.fr>
2024-12-05 12:26:41 +0100

    ♻️ TSE-03: UTILS > Migration of array to TS

---
+++ src/utils/array.ts
@@ -0,0 +1,21 @@
+export const deepClone = <T>(array: T[]): T[] => {
+    return JSON.parse(JSON.stringify(array))
+}
+
+export const findObjectById = <T extends { id: string }>(id: string, array: T[]) => {
+    return array.find((itemInArray) => itemInArray.id === id)
+}
+
+export const findIndexById = <T extends { id: string }>(idWithUnknownIndex: string, array: T[]) => {
+    return array.findIndex(
+        (itemInArray) => itemInArray.id === idWithUnknownIndex
+    )
+}
+
+export const removeObjectById = <T extends { id: string }>(idOfItemToRemove: string, array: T[]) => {
+    return array.filter((item) => item.id !== idOfItemToRemove)
+}
+
+export const isEmpty = (array: []) => {
+    return array.length === 0
+}
