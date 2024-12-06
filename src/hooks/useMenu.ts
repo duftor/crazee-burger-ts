d46ee76c59708b05df7171f61540bb140fc9120c
Victor-Dufayet-Planity <victor.dufayet@planity.com>
2024-12-06 19:53:05 +0100

    ♻️  TSE-04: HOOKS > Migration of useMenu to TS

--- src/hooks/useMenu.ts
+++ src/hooks/useMenu.ts
@@ -2,12 +2,14 @@ import { useState } from "react"
 import { fakeMenu } from "../fakeData/fakeMenu"
 import { deepClone } from "../utils/array"
 import { syncBothMenus } from "../api/product"
+import { Product } from "../types/product"
+import { ID } from "../types/global"
 
 export const useMenu = () => {
-    const [menu, setMenu] = useState()
+    const [menu, setMenu] = useState<Product[]>([]) // TODO: Demander à Vi si c'est pas mieux avec un if (on lance pas les fonctions pour un tableau vide => inutile)
 
     // comportements (gestionnaire de state ou "state handlers")
-    const handleAdd = (newProduct, username) => {
+    const handleAdd = (newProduct: Product, username: string) => {
         // 1. copie du tableau
         const menuCopy = deepClone(menu)
 
@@ -19,7 +21,7 @@ export const useMenu = () => {
         syncBothMenus(username, menuUpdated)
     }
 
-    const handleDelete = (idOfProductToDelete, username) => {
+    const handleDelete = (idOfProductToDelete: ID, username: string) => {
         //1. copy du state
         const menuCopy = deepClone(menu)
 
@@ -34,7 +36,7 @@ export const useMenu = () => {
         syncBothMenus(username, menuUpdated)
     }
 
-    const handleEdit = (productBeingEdited, username) => {
+    const handleEdit = (productBeingEdited: Product, username: string) => {
         // 1. copie du state (deep clone)
         const menuCopy = deepClone(menu)
 
@@ -49,7 +51,7 @@ export const useMenu = () => {
         syncBothMenus(username, menuCopy)
     }
 
-    const resetMenu = (username) => {
+    const resetMenu = (username: string) => {
         setMenu(fakeMenu.LARGE)
         syncBothMenus(username, fakeMenu.LARGE)
     }
